31bfee6a35e7f9037b37e73ab24b06cfab632868
Brad <walker.brad26@gmail.com>
2022-05-16 20:01:05 -0500

    functional boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 function App() {
+
+
+  const[backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
-      App
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
+
     </div>
   )
 }
